c7182b360a22bba77e83664d9e156114c34a4766
Shane Lin <shane@Shanes-MacBook-Pro.local>
2025-02-05 13:10:33 -0500

    change to 3-word + alpha postfix

--- user_obfuscation.py
+++ user_obfuscation.py
@@ -4,25 +4,45 @@ pseudonymization.
 
 The problem is that we want to make it harder to match twitter IDs and
 screennames to collected tweet texts, but still convey authorship
-information across the corpus.
+information across the corpus. Replacing the user id and screen name
+with a consistent and unique pseudonym is a reasonable solution.
 
-To perform this pseudonymization, we take a hash (SHA3-224) of each
-UTF-8 encoded user ID and then use the first 60 bits to look up 5 words
-in the EFF Diceware long word list to serve as a pseudonym.
+Ideally, a pseudonym should be legible at a glance and not too long in
+length.  In this pseudonymization example, we take a hash (SHA3-224) of
+each UTF-8 encoded user ID and then use the first 36 bits to look up 3
+words in the EFF Diceware long word list.
+
+One immediate problem with this approach is that we need a low
+probability of a hash collision, which would cause two users to share
+the same pseudonym. 
 
 The odds of a hash collision can be approximated by:
 P≈1-e^(-(n^2/2H)) 
 (per https://en.wikipedia.org/wiki/Birthday_problem#Approximations)
 
 The charlottesville_2017_0814.json dataset contains 4735052 tweets and
-1700875 unique authors. 
+1700875 unique authors. Conservatively rounding the number of authors up
+to 2 million across all A11/12 datasets, using only three words
+virtually guarantees a collision somewhere in the dataset even if the
+probability that any given user has a doppelganger is relatively low.
 
-Conservatively rounding the number of authors up to 2 million across
-all A11/12 datasets, this means there is a roughly 1 in 5780 chance of
-any collision and roughly 1 in 576 billion chance that any given user
-ID will have a doppelganger. Increasing the obfuscated name to 6 words
-(72 bits) decreases the collision chance to 1 in 23 million while
-reducing the name to 4 words (48 bits) increases it to 1 in 141.
+Increasing the pseudonym to 6 words (72 bits) decreases the collision
+chance to 1 in 23 million, but a 6-word pseudonym is pretty unwieldy.
+We can also increase the size of the word list to buy more entropy, but
+the nice thing about the EFF diceware list is that it's been sanitized
+of problematic words (insults, profanity, slurs, and words evocative of
+trauma and abuse).
+
+Instead, we can reduce the colision chance by appending an alphanumeric
+postfix to serve as a sort of checksum. 
+
+Using a base 33 alphabet (3-Z, since 0,1, and 2 all have similar-looking 
+letters), we can form a 5-character postfix using the next 25 bits of
+the hash. With 61 bits of entropy, the overall chance of a hash
+collision in our user set is reduced to under 1 in 11000.
+
+Each pseudonym would be something in this general format:
+@exceeding-cut-fascism-E5FQP
 
 SHA3 is a crytographic hash, so knowing how we do all of this doesn't
 in itself allow this process to be reversed. However, although SHA3 is
@@ -47,7 +67,25 @@ import json
 import hashlib
 
 # This should be secret
-PEPPER = "gaodJx4W0neaLw5c-"
+PEPPER = "gaodJx4W0neaLw5c"
+
+
+def bin_to_base33(binary):
+    """
+    Convert a binary string to a readable base 33 (3-Z) string.
+    (0, 1, 2 have similar-looking latin characters, so we omit them)
+    """
+    decimal_value = int(binary,2)  # Convert bin to decimal
+    base26_chars = '3456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if decimal_value == 0:
+        return "0"
+
+    base26 = ""
+    while decimal_value:
+        decimal_value, remainder = divmod(decimal_value, 26)
+        base26 = base26_chars[remainder] + base26
+
+    return base26
 
 # Import the counter object from Python's collections module
 # We could have just imported collections, but we'd have to refer to Counter as collections.Counter every time
@@ -78,12 +116,14 @@ obufscated_users  = {}
 for id in user_ids:
     hex = hashlib.sha3_224((PEPPER+str(id)).encode('utf-8')).hexdigest()
     hashes = []
-    for i in range(5):
+    for i in range(3):
         hashes.append(hex[3*i:3*(i+1)])
     dicewords = []
     for hash in hashes:
         dicewords.append(wordlist[int(hash,base=16)])
-    obufscated_users[id] = "-".join(dicewords)
+    alpha = bin_to_base33(bin(int(hex[9:],base=16))[:25])
+    postfix = "0"*(5-len(alpha))+alpha
+    obufscated_users[id] = "-".join(dicewords)+"-"+postfix
 
 # #save the result
 with open("data/user_obfuscation.txt", "w") as outfile:
